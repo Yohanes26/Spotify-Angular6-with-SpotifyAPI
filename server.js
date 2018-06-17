0fb10f3499ea8f7d6e1bbf34218e97188332e990
Yohanes Lopes <yohaneslopes@gmail.com>
2018-06-17 15:39:56 -0300

    Alteracao de porta e path

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use('/', express.static(__dirname + '/dist'));
 
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, './src/index.html');
-    res.sendFile(index);
-});
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
