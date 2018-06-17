4caf5ff5183ec5d3b0b500ea7b1d352bb631ebf3
Yohanes Lopes <yohaneslopes@gmail.com>
2018-06-17 17:09:34 -0300

    Ajuste em serverjs

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, './src/index.html'));
 });
 
 // Initialize the app.
