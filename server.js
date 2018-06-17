a83a08925f81d625f31cf5d48ad03b7d1e5dafce
Yohanes Lopes <yohaneslopes@gmail.com>
2018-06-17 16:55:34 -0300

    Alteracao server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Install express server
+/* //Install express server
 const express = require('express');
 const app = express();
 
@@ -7,3 +7,27 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 3000);
+
+ */
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+module.exports = app;
