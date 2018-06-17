d99a34e34c79c76326d5a097a352f1482f27fb4e
Yohanes Lopes <yohaneslopes@gmail.com>
2018-06-17 15:46:24 -0300

    Alteracao em server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use('/', express.static(__dirname + './src/index.html'));
+app.use(express.static(__dirname + './src'));
 
 
 // Start the app by listening on the default Heroku port
