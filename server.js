3950d1b7e989e358aa6d14a500dac904431191e8
Yohanes Lopes <yohaneslopes@gmail.com>
2018-06-17 16:59:56 -0300

    Ajuste no caminho de dist

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 // Initialize the app.
