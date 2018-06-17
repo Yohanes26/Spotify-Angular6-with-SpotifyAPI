d05d6acc648e8a134380c1e0d70995dc44dfa1d8
Yohanes Lopes <yohaneslopes@gmail.com>
2018-06-17 15:12:22 -0300

    Ajuste em server

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-app.get('/', function(res) {
+app.get('/', function(req, res) {
     res.sendfile('./src/index.html');  
 });
